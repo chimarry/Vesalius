664b77fcde48085398efb7fa413f1130d278e348
Marija Novakovic <marija.vanja.novakovic@gmail.com>
2020-10-13 11:25:31 +0200

    Add design images and activity log window

---
+++ StandardUser/src/pro/artse/user/controllers/ActivityLogController.java
@@ -0,0 +1,52 @@
+package pro.artse.user.controllers;
+
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import pro.artse.user.models.ActivityLog;
+
+/**
+ * Interacts with activity log view.
+ * 
+ * @author Marija
+ *
+ */
+public class ActivityLogController implements Initializable {
+
+	@FXML
+	private TableView<ActivityLog> activityLog;
+
+	@FXML
+	private TableColumn<ActivityLog, String> logInTime;
+
+	@FXML
+	private TableColumn<ActivityLog, String> logOutTime;
+
+	@FXML
+	private TableColumn<ActivityLog, Double> totalTimeSpent;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		LocalDateTime logInAt = LocalDateTime.now();
+		ArrayList<ActivityLog> activityLogs = new ArrayList<>();
+		ObservableList<ActivityLog> activityLogs2 = FXCollections.observableArrayList(activityLogs);
+		activityLogs.add(new ActivityLog(logInAt, logInAt.plusMinutes(30)));
+		activityLogs.add(new ActivityLog(logInAt.plusMinutes(40), logInAt.plusMinutes(50)));
+		activityLogs.add(new ActivityLog(logInAt.plusMinutes(300), logInAt.plusMinutes(500)));
+
+		logInTime.setCellValueFactory(new PropertyValueFactory<>("LogInAt"));
+		logOutTime.setCellValueFactory(new PropertyValueFactory<>("LogOutAt"));
+		totalTimeSpent.setCellValueFactory(new PropertyValueFactory<>("TotalTime"));
+		activityLog.setItems(activityLogs2);
+	}
+}
---
+++ StandardUser/src/pro/artse/user/models/ActivityLog.java
@@ -0,0 +1,106 @@
+package pro.artse.user.models;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+
+/**
+ * Class that holds data for user's activity in application.
+ * 
+ * @author Marija
+ *
+ */
+public class ActivityLog {
+
+	private static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+	private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT);
+
+	/**
+	 * When did user log in?
+	 */
+	private String logInAt;
+
+	/**
+	 * When did user log out?
+	 */
+	private String logOutAt;
+
+	/**
+	 * How much time user spent in application?
+	 */
+
+	private double totalTime;
+
+	/**
+	 * Creates activity log object from strings. If strings cannot be parsed, result
+	 * is set to Unknown.
+	 * 
+	 * @param logInAt  When did user log in? (Expects date-time format)
+	 * @param logOutAt When did user log out? (Expects date-time format)
+	 */
+	public ActivityLog(String logInAt, String logOutAt) {
+		super();
+		try {
+			LocalDateTime time1 = LocalDateTime.parse(logInAt);
+			LocalDateTime time2 = LocalDateTime.parse(logOutAt);
+			initialize(time1, time2);
+		} catch (Exception e) {
+			this.logOutAt = "Unknown";
+			this.logInAt = "Unknown";
+		}
+	}
+
+	/**
+	 * Creates activity log object from date-time objects. If strings cannot be
+	 * parsed, result is set to Unknown.
+	 * 
+	 * @param logInAt  When did user log in?
+	 * @param logOutAt When did user log out?
+	 */
+	public ActivityLog(LocalDateTime logInAt, LocalDateTime logOutAt) {
+		super();
+		initialize(logInAt, logOutAt);
+	}
+
+	public static DateTimeFormatter getFormatter() {
+		return formatter;
+	}
+
+	public String getLogInAt() {
+		return logInAt;
+	}
+
+	public String getLogOutAt() {
+		return logOutAt;
+	}
+
+	public double getTotalTime() {
+		return totalTime;
+	}
+
+	/**
+	 * Returns time spent in hours.
+	 * 
+	 * @param logInAt  Begin.
+	 * @param logOutAt End.
+	 * @return
+	 */
+	private double calculateTimeSpent(LocalDateTime logInAt, LocalDateTime logOutAt) {
+		long secs = ChronoUnit.SECONDS.between(logInAt, logOutAt);
+		return secs / 3600.0;
+	}
+
+	/**
+	 * Initializes log in and log out time with proper formatter, and sets total
+	 * time spent in application.
+	 * 
+	 * @param logInAt  When did user log in?
+	 * @param logOutAt When did user log out?
+	 */
+	private void initialize(LocalDateTime logInAt, LocalDateTime logOutAt) {
+		this.totalTime = calculateTimeSpent(logInAt, logOutAt);
+		this.logInAt = formatter.format(logInAt);
+		this.logOutAt = formatter.format(logOutAt);
+	}
+}
