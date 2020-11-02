3d14c5726b23141665ce19aa1d749f66f9d0a8cd
Marija Novakovic <marija.vanja.novakovic@gmail.com>
2020-11-02 11:40:33 +0100

    Initialize desktop application for the medical staff

---
+++ MedicalStaff/src/pro/artse/medicalstaff/controllers/StartupApplication.java
@@ -0,0 +1,25 @@
+package pro.artse.medicalstaff.controllers;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class StartupApplication extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * The main() method is ignored in correctly deployed JavaFX application. main()
+	 * serves only as fallback in case the application can not be launched through
+	 * deployment artifacts, e.g., in IDEs with limited FX support. NetBeans ignores
+	 * main().
+	 *
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ MedicalStaff/src/pro/artse/medicalstaff/util/package-info.java
@@ -0,0 +1,1 @@
+package pro.artse.medicalstaff.util;
